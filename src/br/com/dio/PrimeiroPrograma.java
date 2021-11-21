60c1c7839e06a71eba90e280884ac46c3c68694d
EliasLobobm <eliaslobbo@gmail.com>
2021-11-21 15:07:19 -0300

    Criando Class Livro

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -5,5 +5,43 @@ public class PrimeiroPrograma {
         System.out.println("Elias Lobo");
         System.out.println("Elias Lobo");
         System.out.println("Elias Lobo");
+
+        Livro livro = new Livro("Elias Lobo", 300);
+        System.out.println(livro);
+    }
+
+}
+
+class Livro {
+    private String nome;
+    private Integer numPagina;
+
+    public Livro(String nome, Integer numPagina) {
+        this.nome = nome;
+        this.numPagina = numPagina;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPagina() {
+        return numPagina;
+    }
+
+    public void setNumPagina(Integer numPagina) {
+        this.numPagina = numPagina;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPagina=" + numPagina +
+                '}';
     }
 }
